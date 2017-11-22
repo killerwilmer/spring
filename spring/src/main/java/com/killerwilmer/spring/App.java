d13a6e84038d0b4c750976b7f009c80ffd1b0198
Wilmer <killerwilmer@gmail.com>
2017-11-22 15:56:18 -0500

    Colecciones - List

--- spring/src/main/java/com/killerwilmer/spring/App.java
+++ spring/src/main/java/com/killerwilmer/spring/App.java
@@ -7,12 +7,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.killerwilmer.beans.AppConfig;
 import com.killerwilmer.beans.AppConfig2;
+import com.killerwilmer.beans.Ciudad;
 import com.killerwilmer.beans.Mundo;
 import com.killerwilmer.beans.Persona;
 
 public class App {
 
 	public static void main(String[] args) {
+		
+		// Por anotaciones
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
 		applicationContext.register(AppConfig.class);
 		applicationContext.register(AppConfig2.class);
@@ -27,7 +30,12 @@ public class App {
 		// Por xml
 		ApplicationContext applicationContextXml = new ClassPathXmlApplicationContext("com/killerwilmer/xml/beans.xml");
 		Persona persona = (Persona) applicationContextXml.getBean("personaBean");
-		System.out.print(persona.getId() + " " + persona.getNombre() + " " + persona.getApodo() + " " + persona.getPais().getNombre() + " " + persona.getPais().getCiudad().getNombre());
+		
+		String nombresCiudades = ""; 
+		for (Ciudad ciudad : persona.getPais().getCiudades()) {
+			nombresCiudades += ciudad.getNombre() + "-";
+		}
+		System.out.print(persona.getId() + " " + persona.getNombre() + " " + persona.getApodo() + " " + persona.getPais().getNombre() + " " + nombresCiudades);
 		((ConfigurableApplicationContext)applicationContextXml).close();
 	}
 

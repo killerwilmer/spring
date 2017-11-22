140c0eca114dd16f54c4973c34a4609f166a8f4a
Wilmer <killerwilmer@gmail.com>
2017-11-22 13:18:46 -0500

    Inyección por Constructor

--- spring/src/main/java/com/killerwilmer/spring/App.java
+++ spring/src/main/java/com/killerwilmer/spring/App.java
@@ -8,11 +8,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.killerwilmer.beans.AppConfig;
 import com.killerwilmer.beans.AppConfig2;
 import com.killerwilmer.beans.Mundo;
+import com.killerwilmer.beans.Persona;
 
 public class App {
 
 	public static void main(String[] args) {
-		//ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/killerwilmer/xml/beans.xml");
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
 		applicationContext.register(AppConfig.class);
 		applicationContext.register(AppConfig2.class);
@@ -23,6 +23,12 @@ public class App {
 		System.out.print(mundo.getSaludo());
 		
 		((ConfigurableApplicationContext)applicationContext).close();
+		
+		// Por xml
+		ApplicationContext applicationContextXml = new ClassPathXmlApplicationContext("com/killerwilmer/xml/beans.xml");
+		Persona persona = (Persona) applicationContextXml.getBean("persona");
+		System.out.print(persona.getId() + " " + persona.getNombre() + " " + persona.getApodo());
+		((ConfigurableApplicationContext)applicationContextXml).close();
 	}
 
 }

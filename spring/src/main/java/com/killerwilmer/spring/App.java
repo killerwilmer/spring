50895bbec6730560cdce328276630c6ae4544e2c
Wilmer <killerwilmer@gmail.com>
2017-11-21 22:37:02 -0500

    Agregar 2 archivos de configuracion

--- spring/src/main/java/com/killerwilmer/spring/App.java
+++ spring/src/main/java/com/killerwilmer/spring/App.java
@@ -6,14 +6,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.killerwilmer.beans.AppConfig;
+import com.killerwilmer.beans.AppConfig2;
 import com.killerwilmer.beans.Mundo;
 
 public class App {
 
 	public static void main(String[] args) {
 		//ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/killerwilmer/xml/beans.xml");
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-		Mundo mundo = (Mundo) applicationContext.getBean("mundo");
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		applicationContext.register(AppConfig.class);
+		applicationContext.register(AppConfig2.class);
+		applicationContext.refresh();
+		
+		Mundo mundo = (Mundo) applicationContext.getBean("marte");
 		
 		System.out.print(mundo.getSaludo());
 		
